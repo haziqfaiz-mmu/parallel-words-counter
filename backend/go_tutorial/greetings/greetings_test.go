cd755ef0b6d777f7b8cd63ca3f6ff47888ddb43c
haziqfaiz <78082475+haziqfaiz-mmu@users.noreply.github.com>
2023-08-11 09:15:27 +0000

    added tutorial

---
+++ backend/go_tutorial/greetings/greetings_test.go
@@ -0,0 +1,22 @@
+package greetings
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestHelloName(t *testing.T) {
+	name := "Gladys"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg, err := Hello("Glady")
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
+
+func testHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
